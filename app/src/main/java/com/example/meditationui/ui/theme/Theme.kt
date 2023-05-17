eb6d2ef13755fa0509c2d09f66fbfd612f75615a
rieadaits <rieadaits21@gmail.com>
2023-05-17 15:22:30 +0600

    feature section desing done

--- app/src/main/java/com/example/meditationui/ui/theme/Theme.kt
+++ app/src/main/java/com/example/meditationui/ui/theme/Theme.kt
@@ -3,6 +3,9 @@ package com.example.meditationui.ui.theme
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 
 @Composable
@@ -10,6 +13,14 @@ fun MeditationUiTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
+
+    val systemUiController = rememberSystemUiController()
+    SideEffect {
+        systemUiController.setStatusBarColor(
+            color = DeepBlue,
+            darkIcons = false
+        )
+    }
 
     MaterialTheme(
         shapes = Shapes,
